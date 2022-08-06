ffb4d6b16d6f3b38344ad46899b87e875195715c
manoloacademia <luispablosegovia@icloud.com>
2022-08-06 16:14:35 -0300

    Se crea modelo Wine

--- wine_project/wines/models.py
+++ wine_project/wines/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Wine(models.Model):
+    name = models.CharField(max_length=50)
+    winery = models.CharField(max_length=50)
+    variety = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
