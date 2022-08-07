68f5b9fee57bd2bde588e89ae92ab4803ecd8b23
manoloacademia <luispablosegovia@icloud.com>
2022-08-07 09:32:51 -0300

    Se añade vista de creación de vinos.

--- wine_project/wines/views.py
+++ wine_project/wines/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from .models import Wine
+from django.views.generic.edit import CreateView
 
 def wines(request):
     wine_list = Wine.objects.all()
     context = {'wine_list': wine_list}
     return render(request, 'wines/wine_list.html', context=context)
 
-
+class WineCreate(CreateView):
+    model = Wine
+    fields = '__all__'
