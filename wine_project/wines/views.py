30949dd00fd5b3069e93ac4b0e41b5fd9de1afa0
manoloacademia <luispablosegovia@icloud.com>
2022-08-07 10:10:10 -0300

    Se agrega pagina de search para nombres de vinos.

--- wine_project/wines/views.py
+++ wine_project/wines/views.py
@@ -11,3 +11,9 @@ class WineCreate(CreateView):
     model = Wine
     fields = '__all__'
     success_url = '/'
+
+def search_products(request):
+    search = request.GET['search']
+    wines = Wine.objects.filter(name__icontains=search)  #Trae los que cumplan la condicion
+    context = {'wines':wines}
+    return render(request, 'wines/search.html', context=context)
