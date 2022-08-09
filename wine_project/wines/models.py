7edf5a5f48bbd29e97a51626423c21afb17a0284
manoloacademia <luispablosegovia@icloud.com>
2022-08-09 10:30:20 -0300

    Modificacion de templaes de wines

--- wine_project/wines/models.py
+++ wine_project/wines/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Wine(models.Model):
     name = models.CharField(max_length=50)
     winery = models.CharField(max_length=50)
-    variety = models.CharField(max_length=50)
+    varietal = models.CharField(max_length=50)
     age = models.IntegerField()
     price = models.FloatField(default=0)
 
