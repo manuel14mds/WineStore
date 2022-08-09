86254c48529ce7c07ce4fc2b57733e1a5cfe1265
manuel14mds <manuel14mds@gmail.com>
2022-08-08 19:10:02 -0500

    commit first merge

--- wine_project/winery/urls.py
+++ wine_project/winery/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from winery.views import wineries, add_winery
 
 urlpatterns = [    
-    path('addWinery', add_winery, name='add a new winery'),
+    path('addWinery/', add_winery, name='add a new winery'),
     path('wineries/', wineries, name='show all wineries'),
 ]
