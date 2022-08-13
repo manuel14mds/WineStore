f7c056442c4d07bb9d211ef0978f26a5e42c5145
manoloacademia <luispablosegovia@icloud.com>
2022-08-13 16:47:21 -0300

    ForeignKey relationship

--- wine_project/wines/models.py
+++ wine_project/wines/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from varietal.models import varietal
+from winery.models import Winery
 
 class Wine(models.Model):
     name = models.CharField(max_length=50)
-    winery = models.CharField(max_length=50)
-    varietal = models.CharField(max_length=50)
+    winery = models.ForeignKey(Winery, on_delete=models.CASCADE)
+    varietal = models.ForeignKey(varietal, on_delete=models.CASCADE)
     age = models.IntegerField()
     price = models.FloatField(default=0)
     image = models.ImageField(upload_to='wines/', null=True, blank=True)
