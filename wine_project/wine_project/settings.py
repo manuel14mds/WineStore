92af6b332bbb99f2676e1ebb6a2bd7e2fa53a986
manoloacademia <luispablosegovia@icloud.com>
2022-08-12 22:52:41 -0300

    Se agrega mensaje al no encontrar vino

--- wine_project/wine_project/settings.py
+++ wine_project/wine_project/settings.py
@@ -121,7 +121,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/users/login/'
