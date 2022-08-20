43303776e8806abc420c954e7194f593eeba4e2d
manoloacademia <luispablosegovia@icloud.com>
2022-08-20 10:06:26 -0300

    Added about template and functionality.

--- wine_project/wine_project/urls.py
+++ wine_project/wine_project/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from wine_project.views import home
 from django.conf.urls.static import static
 from django.conf import settings
+from .views import about_us
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +27,5 @@ urlpatterns = [
     path('varietal/', include('varietal.urls')),
     path('winery/', include('winery.urls')),
     path('users/', include('users.urls')),
+    path('about/', about_us, name='about'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- wine_project/wine_project/views.py
+++ wine_project/wine_project/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'home.html',context={})
+
+def about_us(request):
+    return render(request,'about.html',context={})
